3cb592a78abb2d266c66cabd424defb30fdd9066
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-09-14 16:59:20 -0500

    Time: 0 ms (100.00%) | Memory: 11.7 MB (80.56%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (auto i : nums) {
+            if (i!=val) {
+                nums[k] = i;
+                k++;
+            }
+        }
+        return k;
+    }
+};
