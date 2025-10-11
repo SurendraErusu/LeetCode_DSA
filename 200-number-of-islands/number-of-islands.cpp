571261dfe20989afa41592c07c7008d7a9db812a
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-10-11 16:56:11 -0500

    Time: 21 ms (91.46%) | Memory: 16.3 MB (69.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,33 +3,26 @@ public:
 
     vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
-    void bfs(vector<vector<char>>& grid, int row, int col) {
-        queue<pair<int,int>> q;
-        q.push({row, col});
-        grid[row][col] = '0';
-        while(!q.empty()) {
-            auto node = q.front();
-            q.pop();
-            int r = node.first;
-            int c = node.second;
-            for (int i = 0; i < dir.size(); i++) {
-                int nr = r+dir[i][0];
-                int nc = c+dir[i][1];
-                if (nr < 0 || nr >= grid.size() || nc < 0 || nc >= grid[0].size() 
-                || grid[nr][nc] == '0') {
-                    continue;
-                }
-                q.push({nr,nc});
-                grid[nr][nc] = '0';
-            }
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (row <0 || row >= grid.size() || col<0 || col>= grid[0].size() 
+        || grid[row][col] == '0') {
+            return;
         }
+        grid[row][col] = '0';
+        for(int i = 0; i < dir.size(); i++) {
+            int r = row+dir[i][0];
+            int c = col+dir[i][1];
+            dfs(grid, r, c);
+        }
+
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int count = 0;
         for (int i = 0; i<grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == '1') {
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
                     count++;
                 }
             }
