075618a1042b8eff2464cfb241cb57269b28e6c2
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-10-11 16:27:14 -0500

    Time: 33 ms (23.67%) | Memory: 23.2 MB (32.12%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{0,-1}, {-1,0}, {0,1}, {1,0}};
-    void dfs(vector<vector<char>>& grid, int row, int col) {
-        if (row<0 || row >= grid.size() || col<0 || col>= grid[0].size() ||
-         grid[row][col]=='0') {
-            return;
-         }
-         grid[row][col] = '0';
-         for (int i = 0; i < 4; i++) {
-            dfs(grid, row+dir[i][0], col+dir[i][1]);
-         }
 
+    vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    void bfs(vector<vector<char>>& grid, int row, int col) {
+        queue<pair<int,int>> q;
+        q.push({row, col});
+        grid[row][col] = '0';
+        while(!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            int r = node.first;
+            int c = node.second;
+            for (int i = 0; i < dir.size(); i++) {
+                int nr = r+dir[i][0];
+                int nc = c+dir[i][1];
+                if (nr < 0 || nr >= grid.size() || nc < 0 || nc >= grid[0].size() 
+                || grid[nr][nc] == '0') {
+                    continue;
+                }
+                q.push({nr,nc});
+                grid[nr][nc] = '0';
+            }
+        }
     }
-
     int numIslands(vector<vector<char>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
         int count = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for (int i = 0; i<grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     count++;
                 }
             }
