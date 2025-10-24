46decf0c4205adf41cb157db18548a91b473a0c7
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-10-24 07:14:31 -0500

    Time: 0 ms (100.00%) | Memory: 11.1 MB (8.13%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int helper (vector<int> nums) {
+        int n  = nums.size();
+        vector<int> dp(n);
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            int pick = nums[i]+dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = max(pick, notPick);
+        }
+        return dp[n-1];
+    }
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+    return max(helper(vector<int>(nums.begin() + 1, nums.end())),
+                        helper(vector<int>(nums.begin(), nums.end() - 1)));
+    }
+};
