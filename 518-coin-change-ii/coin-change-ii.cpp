bd9c6b64d422a8f9abffc3a2f661001f1497cec1
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-10-28 19:55:14 -0500

    Time: 4 ms (90.63%) | Memory: 10.4 MB (84.94%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         vector<long long> dp(amount + 1, 0);
-        dp[0] = 1;  // one way to make 0 amount — take nothing
+        dp[0] = 1;  
 
         for (int coin : coins) {
             for (int j = coin; j <= amount; j++) {
                 dp[j] += dp[j - coin];
-                if (dp[j] > INT_MAX) dp[j] = INT_MAX; // prevent overflow
+                if (dp[j] > INT_MAX) dp[j] = INT_MAX; 
             }
         }
 
