7b6afede0fe26b31159ee13653ac028ae695f388
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-11-12 19:39:50 -0600

    Time: 27 ms (69.42%) | Memory: 44.3 MB (21.15%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long>(m + 1, 0));
+        vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long>(m + 1));
 
         for (int i = 0; i <= n; i++) dp[i][0] = 1;
         for (int j = 1; j <= m; j++) dp[0][j] = 0;
