afe3f8b254f196cf23df816cd7c6f183a20862df
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-11-20 20:33:07 -0600

    Time: 0 ms (100.00%) | Memory: 10.1 MB (98.92%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        vector<bool> dp(n + 1, false);
-        dp[n] = true;  
-
-        for (int i = n - 1; i >= 0; i--) {
+        vector<bool> dp(n+1, false);
+        dp[n] = true;
+        for (int i = s.size()-1; i>= 0; i--) {
             for (int j = 0; j < wordDict.size(); j++) {
                 int len = wordDict[j].size();
-                if (i + len <= n && s.substr(i, len) == wordDict[j]) {
-                    dp[i] = dp[i + len];
+                if (i+len <= s.size() && wordDict[j] == s.substr(i,len)) {
+                    dp[i] = dp[i+len];
+                    //break;
                 }
-                if (dp[i]) break;  
+                if (dp[i]) break; 
+
             }
         }
-
         return dp[0];
     }
 };
