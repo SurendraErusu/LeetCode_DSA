f88373fb552c51d4e3231a618a8d587085fbda81
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2025-12-26 18:27:58 -0500

    Time: 0 ms (100.00%) | Memory: 42 MB (31.65%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector <int>  pre(nums.size(), 1);
+        vector <int> suf(nums.size(), 1);
+        vector <int> res(nums.size());
+        int prefix = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            pre[i] = pre[i-1]*nums[i-1];
+        }
+        int suffix = 1;
+        for (int i = nums.size()-2;  i>=0; i--) {
+            suf[i] = suf[i+1]*nums[i+1]; 
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            res[i] = pre[i]*suf[i];
+        }
+        return res;
+    }
+};
