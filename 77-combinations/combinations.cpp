8bfca9bb29a848b946b3b5494e6756349dce3784
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2026-01-15 22:24:42 -0500

    Time: 43 ms (56.07%) | Memory: 62.6 MB (56.89%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    void helper(int i, int k, const vector<int>& nums,
-                vector<vector<int>>& res, vector<int>& arr) {
-        if ((int)arr.size() > k) return; // optional prune
 
-        if (i == (int)nums.size()) {
-            if ((int)arr.size() == k) res.push_back(arr);
+    void helper (int i, int k, vector<int> &nums, vector<vector<int>> &res, 
+    vector<int> &arr) {
+        if (i == nums.size()) {
+            if (arr.size() == k) {
+                res.push_back(arr);
+            }
             return;
         }
-
         arr.push_back(nums[i]);
-        helper(i + 1, k, nums, res, arr);
+        helper(i+1, k, nums, res, arr);
         arr.pop_back();
-
-        helper(i + 1, k, nums, res, arr);
+        helper(i+1, k, nums, res, arr);
     }
 
     vector<vector<int>> combine(int n, int k) {
         vector<int> nums;
-        nums.reserve(n);
-        for (int x = 1; x <= n; x++) nums.push_back(x);
-
-        vector<vector<int>> res;
         vector<int> arr;
+        vector<vector<int>> res;
+        for (int i = 1; i <= n; i++) {
+            nums.push_back(i);
+        }
         helper(0, k, nums, res, arr);
         return res;
     }
