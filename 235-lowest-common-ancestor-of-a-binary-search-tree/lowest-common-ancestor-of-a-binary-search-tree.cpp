2f56c1899ef9c30d8b3c566d49861e4d421f89b2
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2026-01-18 15:55:07 -0500

    Time: 22 ms (62.34%) | Memory: 23.4 MB (89.88%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,9 +15,10 @@ public:
         if (root->val > p->val && root->val > q->val) {
             return lowestCommonAncestor(root->left, p, q);
         }
-       if (root->val < p->val && root->val < q->val) {
+       else if (root->val < p->val && root->val < q->val) {
             return lowestCommonAncestor(root->right, p, q);
-        }
+        } else {
         return root;
+        }
     }
 };
