7a8053c8c0a7dea83cbcdac25e5d9091d67067c2
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2026-01-17 19:00:03 -0500

    Time: 0 ms (100.00%) | Memory: 12.8 MB (46.47%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,10 +15,10 @@ public:
         if (p == NULL && q == NULL) {
             return true;
         }
-        if (p==NULL || q== NULL) {
+        if (p==NULL || q== NULL || (p->val!=q->val)) {
             return false;
         }
-        return ((p->val==q->val) && (isSameTree(p->left,q->left)) && 
+        return ((isSameTree(p->left,q->left)) && 
         (isSameTree(p->right,q->right)));
     }
 };
