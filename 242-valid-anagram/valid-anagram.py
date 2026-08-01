feaf1421b6a12e978655ac44c37965271c85e747
Surendra Reddy Erusu <143525536+SurendraErusu@users.noreply.github.com>
2026-08-01 16:55:09 -0700

    Time: 23 ms (50.06%) | Memory: 14.4 MB (8.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        d1 = {}
+        d2 = {}
+        for i in range(len(s)):
+            d1[s[i]] = d1.get(s[i],0)+1
+            d2[t[i]] = d2.get(t[i],0)+1
+        
+        return d1 == d2
+
+
+
+        
+        
